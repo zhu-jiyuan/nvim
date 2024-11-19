07077ea758c40862ba5164bb6e494051c74ca37e
JiYuan <olderfe66@gmail.com>
2024-11-19 23:03:47 +0800

    feat(git and ai): add neogit and chat ai.

---
+++ lua/plugins/ai_chat.lua
@@ -0,0 +1,63 @@
+return {
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+        },
+        build = "make tiktoken",          -- Only on MacOS or Linux
+        opts = {
+            debug = false,                -- Enable debugging
+            -- See Configuration section for rest
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+        keys = {
+            { "<c-s>",     "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
+            { "<leader>a", "",     desc = "+ai",        mode = { "n", "v" } },
+            {
+                "<leader>aa",
+                function()
+                    return require("CopilotChat").toggle()
+                end,
+                desc = "Toggle (CopilotChat)",
+                mode = { "n", "v" },
+            },
+            {
+                "<leader>ax",
+                function()
+                    return require("CopilotChat").reset()
+                end,
+                desc = "Clear (CopilotChat)",
+                mode = { "n", "v" },
+            },
+            {
+                "<leader>aq",
+                function()
+                    local input = vim.fn.input("Quick Chat: ")
+                    if input ~= "" then
+                        require("CopilotChat").ask(input)
+                    end
+                end,
+                desc = "Quick Chat (CopilotChat)",
+                mode = { "n", "v" },
+            },
+        },
+        config = function(_, opts)
+            local chat = require("CopilotChat")
+            if pcall(require, "cmp") then
+                require("CopilotChat.integrations.cmp").setup()
+            end
+
+            vim.api.nvim_create_autocmd("BufEnter", {
+                pattern = "copilot-chat",
+                callback = function()
+                    vim.opt_local.relativenumber = false
+                    vim.opt_local.number = false
+                end,
+            })
+
+            chat.setup(opts)
+        end,
+    },
+}
--- lua/plugins/git-plugin.lua
+++ lua/plugins/git-plugin.lua
@@ -1,19 +1,36 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        event = "BufReadPre",
-        config = function()
-            require("gitsigns").setup({
-                -- current_line_blame = true,
-            })
-        end,
-    },
-    {
-        "sindrets/diffview.nvim",
-        cmd = "DiffviewOpen",
-    },
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufReadPre",
+		config = function()
+			require("gitsigns").setup({
+				-- current_line_blame = true,
+			})
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		cmd = "DiffviewOpen",
+	},
 	{
 		"tpope/vim-fugitive",
 		cmd = "Git",
 	},
+	{
+		"NeogitOrg/neogit",
+		cmd = "Neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed.
+			"ibhagwan/fzf-lua", -- optional
+		},
+		keys = {
+			{ "<leader>go", ":Neogit cwd=%:p:h<CR>",               desc = "Uses the repository of the current file" },
+			{ "<leader>gc", ":Neogit commit<CR>",                  desc = "Uses the repository of the current file" },
+			{ "<leader>gs", ":Neogit cwd=%:p:h kind=floating<CR>", desc = "Uses the repository of the current file" },
+		},
+		config = true,
+	}
 }
