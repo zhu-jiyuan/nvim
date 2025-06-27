d6c634bbe528eb6fe2c0d2919d0b38054ed9abbc
jiyuan <olderfe66@gmail.com>
2025-06-27 12:27:49 +0800

    feat(blink-cmp): snippets switch to default.

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -10,6 +10,7 @@ return {
 				'L3MON4D3/LuaSnip',
 				dependencies = { "rafamadriz/friendly-snippets" },
 				version = 'v2.*',
+				build = "make install_jsregexp",
 				config = function()
 					require("luasnip.loaders.from_vscode").lazy_load()
 					require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
@@ -78,7 +79,7 @@ return {
 			},
 
 			-- snippets
-			snippets = { preset = 'luasnip' },
+			snippets = { preset = 'default' },
 			cmdline = {
 				enabled = true,
 				keymap = {
