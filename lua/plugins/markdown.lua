8fad4a26ed43a3f5d72012f479f56013380234e0
JiYuan <olderfe66@gmail.com>
2025-10-06 23:06:15 +0800

    feat(markdown): change markdown plugin.

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,12 +1,14 @@
 return {
 	{
-		"OXY2DEV/markview.nvim",
-		lazy = true, -- Recommended
-		ft = "markdown", -- If you decide to lazy-load anyway
-
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons"
-		}
+		'MeanderingProgrammer/render-markdown.nvim',
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		ft={ 'markdown' },
+		opts = {
+			completions = { lsp = { enabled = true } },
+		},
 	}
 }
