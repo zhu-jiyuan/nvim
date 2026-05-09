f476764b47a20753776fcd3f103523a2a7bb1977
jiyuan <olderfe66@gmail.com>
2026-05-09 11:54:13 +0800

    feat: update format tool

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,32 +1,35 @@
-local disable_filetypes = { c = true, cpp = true, lua = true, python = true, json=true }
+local format_on_save_filetypes = {
+	html = true,
+	typescript = true,
+	typescriptreact = true,
+	-- add filetypes here to opt them into async format-on-save
+}
 
 return {
 	{
 		"stevearc/conform.nvim",
-		opts = {},
 		event = "BufReadPost",
 		config = function()
 			require("conform").setup({
 				formatters_by_ft = {
-					lua        = { "lua_ls" },
-					-- Conform will run multiple formatters sequentially
-					python     = { "ruff"},
-					-- Use a sub-list to run only the first available formatter
-					css        = { "prettier" },
-					javascript = { "prettier" },
-					typescript = { "prettier" },
-					html       = { "prettier" },
-					-- ["*"]      = { "prettier", "codespell" },
+					lua             = { "lua_ls" },
+					python          = { "ruff" },
+					css             = { "biome" },
+					javascript      = { "biome" },
+					javascriptreact = { "biome" },
+					typescript      = { "biome" },
+					typescriptreact = { "biome" },
+					json            = { "biome" },
+					jsonc           = { "biome" },
+					html            = { "prettier" },
 				},
-				format_on_save = function(bufnr)
-					if disable_filetypes[vim.bo[bufnr].filetype] then
+				format_after_save = function(bufnr)
+					if not format_on_save_filetypes[vim.bo[bufnr].filetype] then
 						return
 					end
 					return {
-						-- async = true,
-						timeout_ms = 5000,
-						lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
 						lsp_format = "fallback",
+						timeout_ms = 5000,
 					}
 				end,
 			})
